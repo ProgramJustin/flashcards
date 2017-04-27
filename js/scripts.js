da04a8f4152f11abc6910111027a25ff901c30de
Justin Gomez <programjustin@gmail.com>
2017-04-26 22:37:00 -0700

    fix toggle between topic and description and consolidate query code

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,10 @@
-// WORKING JAVASCRIPT
 $(function() {
-$("#javascript-show, #javascript-hide").click (function() {
-    $("#javascript-show").toggle();
-    $("#javascript-hide").toggle();
-  });
-$("#operators-show, #operators-hide").click (function() {
-      $("#operators-show").toggle();
-      $("#operators-hide").toggle();
-  });
-$("#variables-show, #variables-hide").click (function() {
-      $("#variables-show").toggle();
-      $("#variables-hide").toggle();
-  });
-$("#vnc-show, #vnc-hide").click (function() {
-      $("#vnc-show").toggle();
-      $("#vnc-hide").toggle();
-  });
-$("#functions-show, #functions-hide").click (function() {
-      $("#functions-show").toggle();
-      $("#functions-hide").toggle();
-  });
-$("#methods-show, #methods-hide").click (function() {
-      $("#methods-show").toggle();
-      $("#methods-hide").toggle();
-  });
-  /* TOGGLE-ROW */
+  $(".col-md-4").click(function() {
+  $(this).children("h1").toggle();
+  $(this).children(".hiding").toggle();
 
-  $("").click (function() {
-        $("").toggle();
-        $("").toggle();
-    });
-  $("").click (function() {
-        $("").toggle();
-        $("").toggle();
-    });
-  $("").click (function() {
-        $("").toggle();
-        $("").toggle();
-    });
+
+
+
+  });
 });
-
-
-
-
-
-// EXPERIMENTAL JAVASCRIPT
-// $("#arguments-show, #arguments-hide").click (function() {
-//     $("#arguments-show").toggle();
-//     $("#arguments-hide").toggle();
-//   });
-// $("#parameters-show, #parameters-hide").click (function() {
-//     $("#parameters-show").toggle();
-//     $("#parameters-hide").toggle();
-//   });
-// $("#return-show, #return-hide").click (function() {
-//     $("#return-show").toggle();
-//     $("#return-hide").toggle();
-//   });
-// $("#chaining-show, #chaining-hide").click (function() {
-//     $("#chaining-show").toggle();
-//     $("#chaining-hide").toggle();
-//   });
-// $("strings-show, #strings-hide").click (function() {
-//     $("#strings-show").toggle();
-//     $("#strings-hide").toggle();
-//   });
-// $("#booleans-show, #booleans-hide").click (function() {
-//     $("#booleans-show").toggle();
-//     $("#booleans-hide").toggle();
-//   });
